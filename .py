aff1d15a1c4ba1d2fc6d77c019ed34337a147198
Mer4M3ra <oscarlou89@gmail.com>
2025-08-18 11:09:47 +1000

    started Phase 3

---
+++ .py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv("hate_crimes.csv")
+
+print(df.head   )
--- data_module.py
+++ data_module.py
@@ -1,12 +1,23 @@
 # data_module.py
 import pandas as pd
+df = pd.read_csv("hate_crimes.csv")
 
 def display_dataset_preview():
-    # Use pandas here
+    print(df)
     pass
 
 def update_data_entry():
     # Use pandas here
     pass
 
-# etc...
+def display_visualisation():
+    # pandas code here
+    pass
+
+def search_data():
+    # pandas code here
+    pass
+
+def save_changes():
+    # pandas code here
+    pass
